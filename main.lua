4a44102cf3d4b1d2cd2528c20753ea314a9b7b22
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2022-09-28 12:42:18 +0200

    Update main.lua
    Forgot about BIG letters

--- main.lua
+++ main.lua
@@ -2,10 +2,12 @@ local input = io.open("Input.txt"):read("*all")
 --read file
 first = string.gsub(input,"l","w")
 second = string.gsub(first,"r","w")
+threeakaBIGR = string.gsub(second,"R","W")
+numberfourakaBIGL = string.gsub(threeakaBIGR,"L","W")
 
 -- will use this cuz cant use a table
 local output = io.open("Output.txt", "w")
-output:write(second)
+output:write(numberfourakaBIGL)
 output:close()
 
 -- write "furried" file
