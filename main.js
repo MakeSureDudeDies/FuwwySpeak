7e3666a9f51d20c0b249d54a41de5445dd54fcfb
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2022-12-06 10:32:14 +0100

    Update main.js
    IT was reading from a var called "data"
    like "data = " Hello LR" now it reads from the fs.readFile

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const fs = require('fs'); // fs = file system?????
-fs.readFile('Input.txt', 'utf8', (err, data) => {
+const data = fs.readFile('Input.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
-data = "Hello LR"
 replacing = data.replace(/[lr]/g, "w")
 replacing2 = replacing.replace(/[LR]/g, "W")
 // ^^^^ doing this shit for the third time... but now with "regex", whatever that is...
