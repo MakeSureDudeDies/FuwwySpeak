664cda0455e2629864e227c54b950ee1a307b74d
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2022-12-06 10:25:07 +0100

    Update main.py
    Adding regex

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+import re
 text = open("Input.txt", "r")
 
 # bad coding incoming (i never touch python)
 
 read = text.read()
-first = read.replace("l", "w")
-second = first.replace("r", "w")
-third = second.replace("L", "W")
-fourth = third.replace("R", "W")
+first = re.sub("[lr]", "w", read)
+second = re.sub("[LR]", "W", first)
 
 # im doing the same thing i did in lua but idk if python can replace like with a table
 
 # i do not know well ill just use what i learned like a minute ago (now i see python can do writing like lua)
 output = open("Output.txt", "w")
-output.write(fourth)
+output.write(second)
 output.close() 
 # literally python is a lua clone but a bit harder lmao
