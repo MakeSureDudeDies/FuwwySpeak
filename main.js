eef10963af6596cd4acbcf9f415a74b5432bdd21
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-08-21 14:41:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const fs = require('fs'); // fs = file system?????
-const data = fs.readFile('Input.txt', 'utf8', (err, data) => {
+const data = fs.readFile('IO/Input.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
 replacing = data.replace(/[lr]/g, "w")
 replacing2 = replacing.replace(/[LR]/g, "W")
 // ^^^^ doing this shit for the third time... but now with "regex", whatever that is...
-fs.writeFile('Output.txt', replacing2, err => {
+fs.writeFile('IO/Output.txt', replacing2, err => {
   if (err) {
     console.error(err);
     // file errors = nope
