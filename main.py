867d41879e366fd53675a9f77a0a59729306a9b4
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-08-21 14:41:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-text = open("Input.txt", "r")
+text = open("IO/Input.txt", "r")
 
 # bad coding incoming (i never touch python)
 
@@ -10,7 +10,7 @@ second = re.sub("[LR]", "W", first)
 # im doing the same thing i did in lua but idk if python can replace like with a table
 
 # i do not know well ill just use what i learned like a minute ago (now i see python can do writing like lua)
-output = open("Output.txt", "w")
+output = open("IO/Output.txt", "w")
 output.write(second)
 output.close() 
 # literally python is a lua clone but a bit harder lmao
