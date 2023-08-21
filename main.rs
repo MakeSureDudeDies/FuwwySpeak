b6d346aee9b506db2e1b567903b73c88c6126d10
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-08-21 14:39:59 +0200

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::io::Read;
+use std::fs;
+
+// i dont know what im doing
+
+fn main() {
+    let mut inputfile = File::open("src/Input.txt").unwrap();
+    let mut file_content = String::new();
+    inputfile.read_to_string(&mut file_content).unwrap();
+  
+    let change = file_content.replace("l", "w");
+    let change2 = change.replace("L", "W");
+    let change3 = change2.replace("r", "w");
+    let change4 = change3.replace("R", "W");
+    fs::write("src/Output.txt", change4);
+}
