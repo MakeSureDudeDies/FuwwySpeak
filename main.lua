805d4bd228418114212b1693a3dcc2e3e2f66e19
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2023-08-21 14:41:23 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local input = io.open("Input.txt"):read("*all")
+local input = io.open("IO/Input.txt"):read("*all")
 --read file
 first = string.gsub(input,"l","w")
 second = string.gsub(first,"r","w")
@@ -6,7 +6,7 @@ third = string.gsub(second,"R","W")
 fourth = string.gsub(third,"L","W")
 -- will use this cuz cant use a table
 
-local output = io.open("Output.txt", "w")
+local output = io.open("IO/Output.txt", "w")
 output:write(fourth)
 output:close()
 
