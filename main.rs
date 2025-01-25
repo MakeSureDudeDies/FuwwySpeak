3d6f1a5413e2c9658f8245d9b9c0e1c31a53c521
MakeSureDudeDies <84237951+MakeSureDudeDies@users.noreply.github.com>
2025-01-25 18:30:08 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,17 +1,19 @@
-use std::fs::File;
-use std::io::Read;
 use std::fs;
-
-// i dont know what im doing
-
 fn main() {
-    let mut inputfile = File::open("IO/Input.txt").unwrap();
-    let mut file_content = String::new();
-    inputfile.read_to_string(&mut file_content).unwrap();
-  
-    let change = file_content.replace("l", "w");
-    let change2 = change.replace("L", "W");
-    let change3 = change2.replace("r", "w");
-    let change4 = change3.replace("R", "W");
-    fs::write("IO/Output.txt", change4);
+    let file_content: String = match fs::read_to_string("IO/Input.txt") {
+        Ok(content) => content,
+        _ => panic!("No working"),
+    };
+    let content: Vec<char> = file_content.chars().collect();
+    let mut output: String = String::new();
+
+    for _char in &content {
+        match _char {
+            'r' | 'l' => output.push('w'),
+            'R' | 'L' => output.push('W'),
+            _ => output.push(*_char),
+        };
+    }
+
+    let _ = fs::write("IO/Output.txt",output);
 }
